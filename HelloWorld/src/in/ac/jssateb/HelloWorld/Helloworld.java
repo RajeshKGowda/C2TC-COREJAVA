a0d61a692dc4a3b59dce5f7eaade3b477fbe8376
RajeshKGowda <rajesh.gowdaa15@gmail.com>
2024-07-07 19:10:03 +0530

    First commit

---
+++ HelloWorld/src/in/ac/jssateb/HelloWorld/Helloworld.java
@@ -0,0 +1,11 @@
+package in.ac.jssateb.HelloWorld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO");
+
+	}
+
+}
